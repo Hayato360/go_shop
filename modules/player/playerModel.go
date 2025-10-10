4086ea1d0dd4022635d0f638415d3b9c760eaf6c
Kittipad Kingpudsa <tew965@gmail.com>
2025-10-10 15:47:24 +0700

    Done 52

--- modules/player/playerModel.go
+++ modules/player/playerModel.go
@@ -1,1 +1,13 @@
 package player
+
+import "time"
+
+type (
+	PlayerProfile struct {
+		Id       string `json:"_id"`
+		Email    string `json:"email"`
+		Username string `json:"username"`
+		CreateAt time.Time `json:"created_at"`
+		UpdateAt time.Time `json:"updated_at"`
+	}
+)
