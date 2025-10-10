bf9cfe563cb9a647968e034d04dac22eb5594213
Kittipad Kingpudsa <tew965@gmail.com>
2025-10-10 16:21:41 +0700

    Done 53

--- modules/player/playerModel.go
+++ modules/player/playerModel.go
@@ -4,10 +4,28 @@ import "time"
 
 type (
 	PlayerProfile struct {
-		Id       string `json:"_id"`
-		Email    string `json:"email"`
-		Username string `json:"username"`
+		Id       string    `json:"_id"`
+		Email    string    `json:"email"`
+		Username string    `json:"username"`
 		CreateAt time.Time `json:"created_at"`
 		UpdateAt time.Time `json:"updated_at"`
 	}
+
+	PlayerClaims struct {
+		Id       string `json:"id"`
+		RoleCode int    `json:"role_code"`
+	}
+
+	CreatePlayerReq struct {
+		Email    string `json:"email" form:"email" validate:"required,email,max=255"`
+		Password string `json:"password" form:"password" validate:"required,max=32"`
+		Username string `json:"username" form:"username" validate:"required,max=64"`
+	}
+
+	CreatePlayerTransactionReq struct {
+		PlayerId string  `json:"player_id" validate:"required,max=32"`
+		Amount   float64 `json:"amount" validate:"required"`
+	}
+
+ 
 )
