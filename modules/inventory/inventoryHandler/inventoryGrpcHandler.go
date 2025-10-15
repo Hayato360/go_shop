f5e52c76b17be1d3367c8fdd1d3888c23a7dd457
Kittipad Kingpudsa <tew965@gmail.com>
2025-10-15 10:23:07 +0700

    Done 61

--- modules/inventory/inventoryHandler/inventoryGrpcHandler.go
+++ modules/inventory/inventoryHandler/inventoryGrpcHandler.go
@@ -1,12 +1,16 @@
 package inventoryhandler
 
 import (
+	"context"
+
+	inventoryPb "github.com/Hayato360/go_shop/modules/inventory/inventoryPb"
 	"github.com/Hayato360/go_shop/modules/inventory/inventoryUsecase"
 )
 
 type (
 	inventoryGrpcHandler struct {
 		inventoryUsecase inventoryusecase.InventoryUsecaseService
+		inventoryPb.UnimplementedInventoryGrpcServiceServer
 	}
 )
 
@@ -14,4 +18,8 @@ func NewInventoryGrpcHandler( inventoryUsecase inventoryusecase.InventoryUsecase
 	return &inventoryGrpcHandler{
 		inventoryUsecase: inventoryUsecase,
 	}
+}
+
+func (g *inventoryGrpcHandler) IsAvailableToSell(ctx context.Context, req *inventoryPb.IsAvailableToSellReq) (*inventoryPb.IsAvailableToSellRes, error){
+	return nil , nil
 }
