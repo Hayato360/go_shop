962dfb829a2db4a80adcaa750d5578321646e917
Kittipad Kingpudsa <tew965@gmail.com>
2025-10-16 10:23:28 +0700

    Done 66

--- pkg/database/script/migration.go
+++ pkg/database/script/migration.go
@@ -1,1 +1,33 @@
-package script
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/Hayato360/go_shop/config"
+	"github.com/Hayato360/go_shop/pkg/database/migration"
+)
+
+func main() {
+	ctx := context.Background()
+	_ = ctx
+
+	// Initialize config
+	cfg := config.LoadConfig(func() string{
+		if len(os.Args) < 2 {
+			log.Fatal("Error: .env path is qeiored")
+		}
+		return os.Args[1]
+	}())
+
+	switch cfg.App.Name {
+	case "player" : 
+	case "auth" :
+		migration.AuthMigrate(ctx, &cfg)
+	case "item" :
+	case "inventory" : 
+	case "payment" :
+	
+	}
+}
